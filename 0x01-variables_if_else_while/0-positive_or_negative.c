b5b5d06a34e0ab33b817b60961c558d9f2c79bb5
Kidus Ataklti Brihane <Kiduswsidom77@gmail.com>
2023-08-11 00:18:41 -0700

    1. The last digit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	srand(time(0));
+	a = rand() - RAND_MAX / 2;
+
+	if (a == 0)
+		printf("%d is zero\n", a);
+	else if (a < 0)
+		printf("%d is negative\n", a);
+	else
+		printf("%d is positive\n", a);
+
+	return (0);
+}
