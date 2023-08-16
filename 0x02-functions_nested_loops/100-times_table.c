1f7d6f7cdc8c5ac00335111c697167cde2b3916f
Kidus Ataklti Brihane <Kiduswsidom77@gmail.com>
2023-08-16 00:24:07 -0700

    solve problems

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,115 +1,48 @@
 #include "main.h"
 
 /**
- * print_thousand - print out the digit in the thousand spot
- * @sum: Value to scan for thousand spot
- *
- * Return: Void
- */
-static void print_thousand(int sum)
-{
-	if (sum / 1000 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 1000));
-	}
-}
-
-
-/**
- * print_hundred - print out the digit in the hundred spot
- * @sum: Value to scan for hundred spot
- *
- * Return: Void
- */
-static void print_hundred(int sum)
-{
-	if (sum / 100 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 100));
-	}
-}
-
-
-/**
- * print_tenth - print out the digit in the tenth spot
- * @sum: Value to scan for tenth spot
- *
- * Return: Void
- */
-static void print_tenth(int sum)
-{
-	if (sum / 10 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		if ((sum / 10) > 9)
-		{
-			_putchar('0' + (sum / 10 % 10));
-		}
-		else
-		{
-			_putchar('0' + (sum / 10));
-		}
-	}
-}
-
-
-/**
- * print_base10 - print out the digit in the base10 spot
- * @sum: Value to scan for base10 spot
- *
- * Return: Void
- */
-static void print_base10(int sum)
-{
-	if (sum % 10 == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		_putchar('0' + (sum % 10));
-	}
-}
-
-/**
- * print_times_table - print out the 9 times table
- * @n: Valuse use to generate times tables.
- *
- * Return: void
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int v, h;
+	int i, j, res;
 
-	if (n > 15)
+	if (!(n > 15 || n < 0))
 	{
-		return;
-	}
-	for (h = 0 ; h < n + 1 ; h++)
-	{
-		_putchar('0');
-		for (v = 1 ; v < n + 1 ; v++)
+		for (i = 0; i <= n; i++)
 		{
-			int sum = v * h;
-
-			_putchar(',');
-			print_thousand(sum);
-			print_hundred(sum);
-			print_tenth(sum);
-			print_base10(sum);
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,41 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - the function.
+ *
+ *
+ * Return: Always 0.
  */
-void times_table(void);
+void times_table(void)
 {
-	int i, j, k;
+	int A;
+	int B;
+	int C;
 
-	for (i = 0; i < 10; i++)
+	for (A = 0; A < 10; A++)
 	{
-		for (j = 0; j < 10; j++)
+		for (B = 0; B < 10; B++)
 		{
-			k = j * i;
-			if (j == 0)
+			C = A * B;
+			if (C / 10 != 0)
 			{
-				_putchar(k + '0');
+				if (B != 0)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+				_putchar (C / 10 + '0');
+				_putchar (C % 10 + '0');
 			}
-
-			if (k < 10 && j != 0)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				if (B != 0)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+				_putchar (C  + '0');
 			}
 		}
 		_putchar('\n');
